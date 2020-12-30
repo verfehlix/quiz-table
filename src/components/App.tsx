528e48136a6b66daffa24d6a884d7d7f33f8feef
Felix M <felix.p.magnus@gmail.com>
2020-12-30 18:23:48 +0100

    make it all work

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,29 +1,21 @@
 import React, { useState } from "react"
 import styled from "styled-components"
-import { useLocalStorage } from "../useLocalStorage"
 
 const Title = styled.h1`
     text-align: center;
 `
 
-// "Team Karlsruhe",
-// "Team Wiesloch ",
-// "Team Pawelski",
-// "Team Anna",
-// "Team Max"
-
 const categories = [
-    { name: "Musik", team: "Team ???" },
-    { name: "Sport", team: "Team ???" },
-    { name: "Kultur aus „Aller Welt“", team: "Team ???" },
-    { name: "Wissenschaft", team: "Team ???" },
-    { name: "Klatsch und Tratsch", team: "Team ???" },
-    { name: "Wirtschaft", team: "Team ???" },
-    { name: "Politik", team: "Team ???" },
-    { name: "Film und Fernsehen", team: "Team ???" },
-    { name: "Kunst", team: "Team ???" },
-    { name: "Kulinarisches Auf und Ab", team: "Team ???" },
-    { name: "Technik mit Tech-Nick", team: "Team ???" }
+    { name: "Musik", team: "Team Pawelski" },
+    { name: "Sport", team: "Team Max" },
+    { name: "Kultur aus „Aller Welt“", team: "Team Anna" },
+    { name: "Klatsch und Tratsch", team: "Team Wiesloch" },
+    { name: "Wirtschaft", team: "Team Wiesloch" },
+    { name: "Politik", team: "Team Anna" },
+    { name: "Film und Fernsehen", team: "Team Pawelski" },
+    { name: "Kunst", team: "Team Karlsruhe" },
+    { name: "Kulinarisches Auf und Ab", team: "Team Max" },
+    { name: "Technik mit Tech-Nick", team: "Team Karlsruhe" }
 ]
 
 const levels = ["100", "200", "300", "400", "500", "Schätzfrage"]
@@ -44,7 +36,7 @@ const InnerBorder = styled.div`
 const TableContainer = styled.div`
     display: grid;
 
-    grid-template-columns: repeat(11, 1fr);
+    grid-template-columns: repeat(10, 1fr);
     grid-template-rows: repeat(7, 1fr);
     gap: 5px 5px;
 `
@@ -94,6 +86,9 @@ const LevelItem = styled.div<{ colIndex: number; rowIndex: number; clicked: bool
 
     transition: background-color 250ms linear, transform 250ms linear;
 
+    color: ${(props: { colIndex: number; rowIndex: number; clicked: boolean }) =>
+        props.clicked ? "darkgrey" : "#f5f6fa"}; //
+
     &:hover {
         background-color: ${(props: { colIndex: number; rowIndex: number; clicked: boolean }) =>
             props.clicked ? "#555a66" : "#4066c8;"};
@@ -102,14 +97,34 @@ const LevelItem = styled.div<{ colIndex: number; rowIndex: number; clicked: bool
     }
 `
 
+const TeamDisplay = styled.p`
+    color: grey;
+    font-style: italic;
+    font-weight: lighter;
+`
+
 export const App: React.FC = () => {
-    // const [bla, setBla] = useState<boolean[][]>(
-    //     new Array(categories.length).fill([...new Array(levels.length).fill(false)])
-    // )
-    const [bla, setBla] = useLocalStorage<boolean[][]>(
-        "bla",
-        new Array(categories.length).fill([...new Array(levels.length).fill(false)])
-    )
+    const [storedDataGrid, setStoredDataGrid] = useState<boolean[][]>(() => {
+        try {
+            const item = window.localStorage.getItem("dataGrid")
+            return item
+                ? JSON.parse(item)
+                : new Array(categories.length).fill([...new Array(levels.length).fill(false)])
+        } catch (error) {
+            console.log(error)
+            return new Array(categories.length).fill([...new Array(levels.length).fill(false)])
+        }
+    })
+
+    const setDataGrid = (valueToStore: boolean[][]) => {
+        try {
+            setStoredDataGrid(valueToStore)
+            window.localStorage.setItem("dataGrid", JSON.stringify(valueToStore))
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
         <div className="App">
             <Title>Silvester Quiz 2020</Title>
@@ -118,20 +133,20 @@ export const App: React.FC = () => {
                     <TableContainer>
                         {categories.map((category, index) => (
                             <HeaderItem index={index} key={category.name}>
-                                {category.name}
-                                <br />
-                                <br />
-                                {category.team}
+                                <p>{category.name}</p>
+                                <TeamDisplay>{category.team}</TeamDisplay>
                             </HeaderItem>
                         ))}
                         {categories.map((category, colIndex) =>
                             levels.map((level, rowIndex) => (
                                 <LevelItem
-                                    clicked={bla[colIndex][rowIndex]}
+                                    clicked={storedDataGrid[colIndex][rowIndex]}
                                     onClick={() => {
-                                        const newBla = JSON.parse(JSON.stringify(bla))
-                                        newBla[colIndex][rowIndex] = !bla[colIndex][rowIndex]
-                                        setBla(newBla)
+                                        const newBla = JSON.parse(JSON.stringify(storedDataGrid))
+                                        newBla[colIndex][rowIndex] = !storedDataGrid[colIndex][
+                                            rowIndex
+                                        ]
+                                        setDataGrid(newBla)
                                     }}
                                     colIndex={colIndex}
                                     rowIndex={rowIndex + 2}
@@ -144,6 +159,15 @@ export const App: React.FC = () => {
                     </TableContainer>
                 </InnerBorder>
             </FancyBorder>
+            <button
+                onClick={() => {
+                    setDataGrid(
+                        new Array(categories.length).fill([...new Array(levels.length).fill(false)])
+                    )
+                }}
+            >
+                RESET
+            </button>
         </div>
     )
 }
